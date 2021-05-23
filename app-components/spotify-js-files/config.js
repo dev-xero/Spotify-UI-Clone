02a4ade97ce5869dea52fbf207fbee356081ec4f
CodeNinja-tech <70282966+CodeNinja-tech@users.noreply.github.com>
2021-05-23 23:25:14 +0100

    added minor fixes

--- app-components/spotify-js-files/config.js
+++ app-components/spotify-js-files/config.js
@@ -1,7 +1,7 @@
 window.addEventListener(
   `load`,
   () => {
-    localStorage.setItem(`username`, `Chuka Okafor`);
+    localStorage.setItem(`username`, `Code Ninja 15`);
     localStorage.setItem(`accountType`, `Pro`);
   },
   false
